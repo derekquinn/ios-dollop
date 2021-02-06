a516155539082a974d7865e9209c51fe60186df9
derek quinn <dereq@Troy.local>
2021-02-06 13:53:56 -0500

    Setup navigation controllers and tab bar

--- Dollop/AppDelegate.swift
+++ Dollop/AppDelegate.swift
@@ -1,8 +1,71 @@
 import UIKit
 
+class DollopViewController: UIViewController {
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?){
+        super.init(nibName: nil, bundle: nil)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder){
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func commonInit(){
+        
+    }
+    
+    func setTabBarImage(imageName: String, title: String){
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+    }
+}
+
+class ScanViewController: DollopViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemIndigo
+        title = "Scan"
+    }
+    override func commonInit() {
+        setTabBarImage(imageName: "qrcode", title: "Scan")
+    }
+}
+
+class OrderViewController: DollopViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemIndigo
+        title = "Order"
+    }
+    override func commonInit() {
+        setTabBarImage(imageName: "arrow.up.bin.fill", title: "Order")
+    }
+}
+
+class GiftViewController: DollopViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemIndigo
+        title = "Gift"
+    }
+    override func commonInit() {
+        setTabBarImage(imageName: "gift.fill", title: "Gift")
+    }
+    
+}
+
+class StoreViewController: DollopViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .systemIndigo
+        title = "Store"
+    }
+    override func commonInit() {
+        setTabBarImage(imageName: "location.fill", title: "Stores")
+    }
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOption: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
@@ -10,14 +73,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
         
+        let homeVC = HomeViewController()
+        let scanVC = ScanViewController()
+        let orderVC = OrderViewController()
+        let giftVC = GiftViewController()
+        let storeVC = StoreViewController()
+        
+        let homeNC = UINavigationController(rootViewController: homeVC)
+        let scanNC = UINavigationController(rootViewController: scanVC)
+        let orderNC = UINavigationController(rootViewController: orderVC)
+        let giftNC = UINavigationController(rootViewController: giftVC)
+        let storeNC = UINavigationController(rootViewController: storeVC)
+        
         let tabBarController = UITabBarController()
-        tabBarController.viewControllers = [ViewController()]
+        tabBarController.viewControllers = [homeNC, scanNC, orderNC, giftNC, storeNC]
         
         window?.rootViewController = tabBarController
         
-    
+        
         
         return true
     }
 }
- 
+
---
+++ Dollop/HomeViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class HomeViewController: DollopViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTabBarImage(imageName: "house.fill", title: "Home")
+    }
+    
+}
+
